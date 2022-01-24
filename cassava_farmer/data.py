49b589c8b1d5e627ff3dbedcdc314bad3df955bd
Alec Sharp <alecsharpie@gmail.com>
2022-01-25 03:08:20 +1100

    add option to select local or clou (gcp) data source

--- cassava_farmer/data.py
+++ cassava_farmer/data.py
@@ -1,8 +1,9 @@
-def get_image_generator():
+def get_image_generator(source = "gcp"):
     #'/Users/alecsharp/me/cassava_farmer/raw_data/cassava-leaf-disease-classification/train_images'
-
-    train_path = '../raw_data/cassava-leaf-disease-classification/train_images'
-
+    if source == 'local':
+        train_path = '../raw_data/cassava-leaf-disease-classification/train_images'
+    if source == 'gcp':
+        train_path = 'gs://image-datasets-alecsharpie/cassava_farmer/train_images'
     from tensorflow.keras.utils import image_dataset_from_directory
 
     ds = image_dataset_from_directory(
