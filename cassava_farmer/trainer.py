5737130339cbfa844b3b933ed206e7a31ba2fc1a
Alec Sharp <alecsharpie@gmail.com>
2022-02-27 13:25:10 +1100

    change history file name to not include slashes

--- cassava_farmer/trainer.py
+++ cassava_farmer/trainer.py
@@ -62,14 +62,14 @@ class Trainer:
 
             history = model.fit(
                 train_ds,
-                epochs=5,
+                epochs=1,
                 #batch_size=batch_size,
                 steps_per_epoch=steps_per_epoch,
                 validation_data=val_ds,
                 validation_steps=validation_steps,
                 callbacks=[es]).history
 
-        history_file_name = f'history/{datetime.now().strftime("history_%Y/%m/%d_%H-%M-%S")}'
+        history_file_name = f'history/{datetime.now().strftime("history_%Y-%m-%d_%H-%M-%S")}'
         out_file = open(history_file_name, "w")
         json.dump(history, out_file, indent="")
         out_file.close()
